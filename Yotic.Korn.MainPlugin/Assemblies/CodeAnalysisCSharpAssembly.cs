276c6fa10944cdd7d51d68ff84e556030aa5cb7f
Yotic <yoticcitoy@gmail.com>
2024-12-16 20:52:26 +0300

    Working test version

---
+++ Yotic.Korn.MainPlugin/Assemblies/CodeAnalysisCSharpAssembly.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+class CodeAnalysisCSharpAssembly : IDisposable
+{
+    public CodeAnalysisCSharpAssembly()
+    {
+        MainPlugin.Logger.WriteMessage("CodeAnalysisCSharpAssembly->.ctor");
+
+        var kind = SyntaxFacts.GetKeywordKind("return");
+        MainPlugin.Logger.WriteMessage("kind: " + kind);
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
---
+++ Yotic.Korn.MainPlugin/MainPlugin.cs
@@ -0,0 +1,47 @@
+﻿using Korn;
+using Korn.Plugins.Core;
+using System.Diagnostics.CodeAnalysis;
+
+class MainPlugin : Plugin
+{
+    [AllowNull] public static LocalLogger Logger;
+    [AllowNull] public static string DataDirectory;
+    [AllowNull] static AssemblyWatcher assemblyWatcher;
+
+    public override void OnLoad()
+    {
+        Initialize();
+
+        assemblyWatcher = new AssemblyWatcher();
+        assemblyWatcher.AssemblyLoaded += OnAssemblyLoaded;
+        assemblyWatcher.EnsureAllAssembliesLoaded();
+
+        void Initialize()
+        {
+            DataDirectory = Path.Combine(PluginDirectory, "Data");
+            if (!Directory.Exists(DataDirectory))
+                Directory.CreateDirectory(DataDirectory);
+
+            var logFile = Path.Combine(DataDirectory, "log.txt");
+            Logger = new LocalLogger(logFile);
+
+            Logger.WriteMessage("MainPlugin initialized");
+            KornLogger.WriteMessage("message!");
+        }
+    }
+
+    public override void OnUnload()
+    {
+        assemblyWatcher.Dispose();
+
+        if (codeAnalysisCSharoAssenbly is not null)
+            codeAnalysisCSharoAssenbly.Dispose();
+    }
+
+    CodeAnalysisCSharpAssembly? codeAnalysisCSharoAssenbly;
+    void OnAssemblyLoaded(string name)
+    {
+        if (name == "Microsoft.CodeAnalysis.CSharp")
+            codeAnalysisCSharoAssenbly = new CodeAnalysisCSharpAssembly();
+    }
+}
